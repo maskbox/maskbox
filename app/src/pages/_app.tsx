3db6babd425c8746ec25904fc8df0188efccde37
screfy <screfy@pm.me>
2022-08-30 20:05:37 +0200

    Add Inter font family

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -3,12 +3,27 @@ import { withTRPC } from '@trpc/next';
 import type { AppProps } from 'next/app';
 import superjson from 'superjson';
 import type { AppRouter } from '../server/routers';
+import { globalCss } from '../utils/stitches';
 
 // NOTE: We don't want to use SSR, so relative URL is fine.
 // See: https://trpc.io/docs/ssr
 const TRPC_API_URL = '/api/trpc';
 
+const useGlobalStyles = globalCss({
+	html: {
+		fontFamily: '$sans',
+		WebkitFontSmoothing: 'antialiased',
+		MozOsxFontSmoothing: 'grayscale',
+		WebkitTextSizeAdjust: '100%'
+	},
+	body: {
+		margin: 0
+	}
+});
+
 function App({ Component, pageProps }: AppProps) {
+	useGlobalStyles();
+
 	return <Component {...pageProps} />;
 }
 
--- app/src/utils/stitches.ts
+++ app/src/utils/stitches.ts
@@ -9,4 +9,10 @@ export const {
 	theme,
 	createTheme,
 	config
-} = createStitches();
+} = createStitches({
+	theme: {
+		fonts: {
+			sans: 'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+		}
+	}
+});
