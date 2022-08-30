b2ab09bad3ff912ca4c3525d3394484569079f51
screfy <screfy@pm.me>
2022-08-30 18:09:16 +0200

    Add prisma

---
+++ app/src/utils/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+	var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+	global.prisma ||
+	new PrismaClient({
+		log:
+			process.env.NODE_ENV === 'development'
+				? ['query', 'error', 'warn']
+				: ['error']
+	});
+
+if (process.env.NODE_ENV !== 'production') {
+	global.prisma = prisma;
+}
