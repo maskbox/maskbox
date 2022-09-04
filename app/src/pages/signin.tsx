68cbcc8afeff260321aac0d7e3617c0bacbc89c4
screfy <screfy@pm.me>
2022-09-04 14:35:29 +0200

    Add animation for content

--- app/src/pages/signin.tsx
+++ app/src/pages/signin.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { Button } from '../ui/Button';
 import { Form, useZodForm } from '../ui/Form';
 import { Input } from '../ui/Form/Input';
@@ -6,7 +7,18 @@ import { authChallengeSchema } from '../utils/schema';
 import { styled } from '../utils/stitches';
 import { trpc } from '../utils/trpc';
 
-const StyledContent = styled('div', {
+const variants = {
+	initial: {
+		opacity: 0,
+		scale: 0.96
+	},
+	animate: {
+		opacity: 1,
+		scale: 1
+	}
+};
+
+const StyledContent = styled(motion.div, {
 	display: 'flex',
 	flexDirection: 'column',
 	alignItems: 'center',
@@ -41,48 +53,56 @@ export default function SignIn() {
 		onSuccess: () => form.reset()
 	});
 
-	// TODO: Add animation:
 	return (
-		<StyledContent>
-			{data ? (
-				<>
-					<StyledHeading>Check your email</StyledHeading>
+		<AnimatePresence mode="wait" initial={false}>
+			<StyledContent
+				key={data ? 'signInSuccess' : 'signInForm'}
+				variants={variants}
+				initial="initial"
+				animate="animate"
+				exit="initial"
+				transition={{ duration: 0.25, ease: 'easeInOut' }}
+			>
+				{data ? (
+					<>
+						<StyledHeading>Check your email</StyledHeading>
 
-					<StyledParagraph>
-						We've sent you a temporary sign-in link.
-					</StyledParagraph>
-					<StyledParagraph>
-						Please check your inbox at <StyledStrong>{data.email}</StyledStrong>
-						.
-					</StyledParagraph>
+						<StyledParagraph>
+							We've sent you a temporary sign-in link.
+						</StyledParagraph>
+						<StyledParagraph>
+							Please check your inbox at{' '}
+							<StyledStrong>{data.email}</StyledStrong>.
+						</StyledParagraph>
 
-					<Button
-						variant="ghost"
-						css={{ marginTop: '1.75rem' }}
-						onClick={() => reset()}
-					>
-						Back to sign in
-					</Button>
-				</>
-			) : (
-				<>
-					<StyledHeading>Sign in to Maskbox</StyledHeading>
+						<Button
+							variant="ghost"
+							css={{ marginTop: '1.75rem' }}
+							onClick={() => reset()}
+						>
+							Back to sign in
+						</Button>
+					</>
+				) : (
+					<>
+						<StyledHeading>Sign in to Maskbox</StyledHeading>
 
-					<Form form={form} onSubmit={(data) => mutateAsync(data)} noValidate>
-						<Input
-							type="email"
-							name="email"
-							label="Email"
-							placeholder="Enter your email address..."
-							autoComplete="email"
-						/>
+						<Form form={form} onSubmit={(data) => mutateAsync(data)} noValidate>
+							<Input
+								type="email"
+								name="email"
+								label="Email"
+								placeholder="Enter your email address..."
+								autoComplete="email"
+							/>
 
-						<SubmitButton css={{ width: '100%', marginTop: '1rem' }}>
-							Sign in
-						</SubmitButton>
-					</Form>
-				</>
-			)}
-		</StyledContent>
+							<SubmitButton css={{ width: '100%', marginTop: '1rem' }}>
+								Sign in
+							</SubmitButton>
+						</Form>
+					</>
+				)}
+			</StyledContent>
+		</AnimatePresence>
 	);
 }
