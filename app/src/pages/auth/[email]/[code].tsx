0a4b11f90c6037625d51c78ff745492bb41a9d2c
screfy <screfy@pm.me>
2022-09-06 09:22:15 +0200

    Catch invalid authentication code

--- app/src/pages/auth/[email]/[code].tsx
+++ app/src/pages/auth/[email]/[code].tsx
@@ -1,13 +1,28 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { Button } from '../../../ui/Button';
+import { styled } from '../../../utils/stitches';
 import { trpc } from '../../../utils/trpc';
+
+const StyledContent = styled('div', {
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center'
+});
+
+const StyledHeading = styled('h1', {
+	fontSize: '$lg',
+	fontWeight: '$semibold',
+	marginBottom: '1.75rem'
+});
 
 export default function ExchangeCode() {
 	const { query, push } = useRouter();
 	const email = query.email as string | undefined;
 	const code = query.code as string | undefined;
 
-	const { mutate } = trpc.useMutation('auth.exchangeCode', {
+	const { mutate, isError } = trpc.useMutation('auth.exchangeCode', {
 		onSuccess: () => push('/')
 	});
 
@@ -19,6 +34,19 @@ export default function ExchangeCode() {
 		mutate({ email, code });
 	}, [email, code]);
 
-	// TODO: Catch errors:
-	return <p>Signing you in...</p>;
+	return (
+		<StyledContent>
+			{isError ? (
+				<>
+					<StyledHeading>Invalid authentication code</StyledHeading>
+
+					<Link href="/sign-in">
+						<Button variant="ghost">Back to sign in</Button>
+					</Link>
+				</>
+			) : (
+				<p>Signing you in...</p>
+			)}
+		</StyledContent>
+	);
 }
