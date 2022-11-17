c9a5d4486122f4293f4ed0da04cc4f98b0ca56c9
screfy <screfy@pm.me>
2022-11-17 21:24:02 +0100

    Cleanup code

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,6 +1,2 @@
-export const SESSION_COOKIE_NAME = `${
-	process.env.NODE_ENV === 'production' ? '__Secure-' : ''
-}sid`;
-
 export const MAX_EMAILS_PER_ACCOUNT = 4;
 export const MAX_MASKS_PER_ACCOUNT = 24;
